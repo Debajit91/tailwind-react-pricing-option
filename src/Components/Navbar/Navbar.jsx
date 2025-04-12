1b12e896ab84912f653c63655f443a6acda115a9
Debajit91 <debajitroy544@gmail.com>
2025-04-12 22:48:53 +0600

    dynamically added toggle menu button & toggle menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 const navigationData = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -9,13 +10,28 @@ const navigationData = [
   ];
   
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const links = navigationData.map(route => 
+        <Link key={route.id} route={route}></Link>);
+
+
     return (
-        <nav>
-            <ul className='flex'>
+        <nav className='flex justify-between mx-10'>
+            <span className='flex' onClick={() => setOpen(!open)}>
+                {
+                    open ? <X className='md:hidden'/> :
+                    <Menu className='md:hidden' />
+                }
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+            
+            <h3 className='ml-4'>My Navbar</h3>
+            </span>
+            <ul className='hidden md:flex'>
 
                 {
-                    navigationData.map(route => 
-                    <Link key={route.id} route={route}></Link>)
+                    links
                 }
 
                 {/* {
@@ -27,6 +43,7 @@ const Navbar = () => {
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li> */}
             </ul>
+            <button className='btn'>Sign In</button>
         </nav>
     );
 };
