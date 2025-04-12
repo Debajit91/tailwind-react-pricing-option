bb2436abcae3242b0ac4286bbae4cd3f8f9257a5
Debajit91 <debajitroy544@gmail.com>
2025-04-12 21:39:39 +0600

    added lucide icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import Link from './Link';
+const navigationData = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Services", path: "/services" },
+    { id: 4, name: "Portfolio", path: "/portfolio" },
+    { id: 5, name: "Contact", path: "/contact" },
+  ];
+  
 const Navbar = () => {
     return (
         <nav>
             <ul className='flex'>
-                <li className='mr-10'><a href="/">Home</a></li>
+
+                {
+                    navigationData.map(route => 
+                    <Link key={route.id} route={route}></Link>)
+                }
+
+                {/* {
+                    navigationData.map(route => <li className='mr-10'>
+                        <a href={route.path}>{route.name}</a></li>)
+                } */}
+
+                {/* <li className='mr-10'><a href="/">Home</a></li>
                 <li className='mr-10'><a href="/about">About</a></li>
-                <li className='mr-10'><a href="/blog">Blog</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li> */}
             </ul>
         </nav>
     );
